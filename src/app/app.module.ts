644d3b074e65f88f5d43b4d583453eaa8e9a773a
Shirllie-Apiyo <apraise320@gmail.com>
2022-02-10 01:58:51 +0300

    components&dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './modules/admin/components/dashboard/dashboard.component';
+import { ManageCategoriesComponent } from './modules/admin/components/manage-categories/manage-categories.component';
+import { LoginComponent } from './modules/auth/login/login.component';
+import { RegisterComponent } from './modules/auth/register/register.component';
+import { HomeComponent } from './modules/home/home.component';
+import { SideNavComponent } from './modules/components/side-nav/side-nav.component';
+import { HeaderComponent } from './modules/components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ManageCategoriesComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    SideNavComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
